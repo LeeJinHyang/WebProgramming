f561c0cdc95b668f9bfc5af71979774f9f55cf50
LeeJinHyang <wlsgid2378@naver.com>
2021-05-08 23:57:30 +0900

    테스트를 위한 웹프로젝트

---
+++ WebProject/src/test/java/com/jinhyang/webproject/WebProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jinhyang.webproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
